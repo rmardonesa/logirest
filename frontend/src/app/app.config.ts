dee7dd411c2bdb7e380c6d01cd18b129a12a09d3
rmardonesa <rodrigomardones@protonmail.com>
2026-07-29 14:47:37 -0400

    Arranca el ThemeService de forma explicita al iniciar la aplicacion

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,22 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ThemeService } from './core/services/theme.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    provideAppInitializer(() => {
+      inject(ThemeService);
+    }),
   ],
 };
